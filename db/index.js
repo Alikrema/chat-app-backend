710ab2938018ad7a8538569d822ced4d12641458
alikrema <alimk1741@gmail.com>
2024-03-29 05:44:29 +0200

    fix dotenv path and restructure

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-const config = require('./config');
+const config = require('../config');
 
 const { host, port, user, password, database, dialect } = config.database;
 
--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.port || 3000;
+const config = require('../config/index');
+const cors = require('cors');
+
+const port = config.port;
+console.log('port', config);
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`)
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`)
 });
