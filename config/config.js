f63ede0928b8c538ed4cbc6f7a41cce629259358
alikrema <alimk1741@gmail.com>
2024-03-29 03:13:19 +0200

    establish db connection

---
+++ config/config.js
@@ -0,0 +1,14 @@
+require('dotenv').config({path: '../.env'});
+
+module.exports = {
+    port: process.env.PORT,
+    database: {
+	port: process.env.DB_PORT,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	dialect: process.env.DB_DIALECT
+    },
+    
+}
